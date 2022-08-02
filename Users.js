f2f3f1216fbd687a834e5b8d6295251de48374fa
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2022-08-03 04:29:28 +0530

    /web server usig express

---
+++ Users.js
@@ -0,0 +1,29 @@
+const users=[
+    {
+        id:1,
+        name:"Sangam",
+        email:"Sangam@gmail.com"
+    },
+    {
+        id:2,
+        name:"Krishnam",
+        email:"Krishnam@gmail.com"
+    },
+    {
+        id:3,
+        name:"vivek",
+        email:"vivek@gmail.com"
+    },
+    {
+        id:4,
+        name:"ballu",
+        email:"Ballu@gmail.com"
+    },
+    {
+        id:5,
+        name:"chotu",
+        email:"chotu@gmail.com"
+    },
+]
+
+module.exports=users
